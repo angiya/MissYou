d790ffd1f8170d00ec4f7f2eedcf0e982a2786a8
Angelo Fernando <87890116+angiya@users.noreply.github.com>
2022-05-06 10:43:37 +0530

    Initial commit

---
+++ src/main/java/com/jaylo/missYou/MissYouApplication.java
@@ -0,0 +1,13 @@
+package com.jaylo.missYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaylo/missYou/MissYouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaylo.missYou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
